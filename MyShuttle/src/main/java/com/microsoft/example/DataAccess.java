47d3e31497f492800dbca11a5ae539801188a257
Muneerpasha788 <156910961+Muneerpasha788@users.noreply.github.com>
2025-03-02 01:15:35 +0530

    Update DataAccess.java

--- MyShuttle/src/main/java/com/microsoft/example/DataAccess.java
+++ MyShuttle/src/main/java/com/microsoft/example/DataAccess.java
@@ -1,10 +1,4 @@
 package com.microsoft.example;
-package com.contoso.myshuttle;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
